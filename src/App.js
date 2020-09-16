1ed599d18926b28af769967a0a83b9d127bf4d53
Alec DuBois <after.alec@gmail.com>
2020-09-16 10:16:13 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,83 @@
-import React from 'react';
-import './App.css';
-import { CATEGORIES } from './data'
+import React from "react";
+import "./App.css";
+import { categories, tasks } from "./data";
+import Categories from "./components/Categories";
+import TaskForm from "./components/TaskForm";
+import Tasks from "./components/Tasks";
 
 class App extends React.Component {
-
   state = {
-    tasks: [
-      {
-        text: 'Buy rice',
-        category: 'Food'
+    categories: categories,
+    tasks: tasks,
+    filter: "All",
+    form: {
+      text: "",
+      category: "",
+    },
+  };
+
+  deleteTask = (text) => {
+    this.setState({
+      tasks: this.state.tasks.filter((task) => task.text !== text),
+    });
+  };
+
+  selectCategory = (category) => {
+    this.setState({
+      filter: category,
+    });
+  };
+
+  handleChange = (event) => {
+    console.log(event.target);
+    this.setState({
+      form: {
+        ...this.state.form,
+        [event.target.name]: event.target.value,
       },
-      {
-        text: 'Save a tenner',
-        category: 'Money'
-      },
-      {
-        text: 'Build a todo app',
-        category: 'Code'
-      },
-      {
-        text: 'Build todo API',
-        category: 'Code'
-      },
-      {
-        text: 'Get an ISA',
-        category: 'Money'
-      },
-      {
-        text: 'Cook rice',
-        category: 'Food'
-      },
-      {
-        text: 'Tidy house',
-        category: 'Misc'
-      }
-    ]
-  }
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        { text: this.state.form.text, category: this.state.form.category },
+      ],
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <h2>My tasks</h2>
+        <h2>Taskiron School</h2>
+        <Categories
+          categories={this.state.categories}
+          selected={this.state.filter}
+          handleClick={this.selectCategory}
+        />
+        <div className="tasks">
+          <h5>Tasks</h5>
+          <TaskForm
+            categories={this.state.categories.filter(
+              (category) => category !== "All"
+            )}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            {...this.state.form}
+          />
+          <Tasks
+            filter={this.state.filter !== "All" ? this.state.filter : false}
+            tasks={this.state.tasks}
+            categories={this.state.categories}
+            handleClick={this.deleteTask}
+          />
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
---
+++ src/components/Task.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Task({ text, category, handleClick }) {
+  return (
+    <div className="task">
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={() => handleClick(text)} className="delete">
+        X
+      </button>
+    </div>
+  );
+}
+
+export default Task;
